0af8654c8ca764b05a7c6e67dc6d81f20d7087af
7kokos <7kokos.kok@gmail.com>
2016-01-04 22:16:40 +0200

    Исправление ошибок JS 7-8
    Исправление ошибок JS 7-8

--- GoIT/markup/home/lesson-js-8/js/script.js
+++ GoIT/markup/home/lesson-js-8/js/script.js
@@ -1,39 +1,40 @@
 
 $(function() {
-	$('<div></div>').appendTo('body').addClass('container');
-	$('<form></form>').appendTo('.container').addClass('form-horizontal');
+	// $('<div></div>').appendTo('body').addClass('container');
+	// $('<form></form>').appendTo('.container').addClass('form-horizontal');
 	
-	for (var i = 1; i < 4; i++) {
-		$('<div></div>').appendTo('.form-horizontal').addClass('form-group');
-		$('<label></label>').appendTo('.form-group:nth-child(' + i + ')').addClass('col-sm-2 control-label')
-		.attr({
-			'for':'text'+i}	
-			);
-		$('<div></div>').appendTo('.form-group:nth-child(' + i + ')').addClass('col-sm-10');
-		$('<input></input>').appendTo('.form-group:nth-child(' + i + ')'+'> .col-sm-10').addClass('form-control')
-		.attr({
-			'type':'text',
-			'id':'text'+i,
-			'placeholder': 'Please enter this field'
+	// for (var i = 1; i < 4; i++) {
+	// 	$('<div></div>').appendTo('.form-horizontal').addClass('form-group');
+	// 	$('<label></label>').appendTo('.form-group:nth-child(' + i + ')').addClass('col-sm-2 control-label')
+	// 	.attr({
+	// 		'for':'text'+i}	
+	// 		);
+	// 	$('<div></div>').appendTo('.form-group:nth-child(' + i + ')').addClass('col-sm-10');
+	// 	$('<input></input>').appendTo('.form-group:nth-child(' + i + ')'+'> .col-sm-10').addClass('form-control')
+	// 	.attr({
+	// 		'type':'text',
+	// 		'id':'text'+i,
+	// 		'placeholder': 'Please enter this field'
 
-		});
-	};
+	// 	});
+	// };
 
-	$('.form-group:first-child > label').html('Firstname');
-	$('.form-group:nth-child(2) > label').html('Lastname');
-	$('.form-group:nth-child(3) > label').html('Address');
+	// $('.form-group:first-child > label').html('Firstname');
+	// $('.form-group:nth-child(2) > label').html('Lastname');
+	// $('.form-group:nth-child(3) > label').html('Address');
 
-	$('<button>Show all tooltips</button>').appendTo('form').addClass('btn btn-lg btn-default');
+	// $('<button>Show all tooltips</button>').appendTo('form').addClass('btn btn-lg btn-default');
 	
 
 	var k = 0;	
+
 	$('button').on('click', (function(e){
 		e.preventDefault();
 		console.log($('span'));
-		if (k ===0) {
+		if (k===0) {
 			for (var i = 1; i < 4; i++) {
 				$('.form-group:nth-child(' + i + ')'+'> .col-sm-10')
-				.append($('<span>Please enter ' + ($('.form-group:nth-child(' + i + ')'+'> .col-sm-10').parent().find('label').html()) + '</span>').addClass('tooltips'));
+				.append($('<span>Please enter ' + ($('.form-group:nth-child(' + i + ')').find('.form-control').attr('title')) + '</span>').addClass('tooltips'));
 				};
 		
 			k++;
@@ -44,16 +45,18 @@ $(function() {
 				};
 
 		}));
+	
 	$('.col-sm-10').hover(
   		function() {
   		$('span').remove();
   		k=0;
-  	   	$(this).append($('<span>Please enter ' + ($(this).parent().find('label').html()) + '</span>').addClass('tooltips'));
+  	   	$(this).append($('<span>Please enter ' + ($(this).find('.form-control').attr('title')) + '</span>').addClass('tooltips'));
     
   		}, function() {
     		$(this).find( 'span:last' ).remove();
   		}
 	);
+
 
 });
 
