3d969a3d222e12d87f437f7ae5bae35da7d5acd6
7kokos <7kokos.kok@gmail.com>
2016-02-08 22:53:53 +0200

    added lesson 17-18
    added lesson 17-18

--- GoIT/markup/home/lesson-js-17-18/Gruntfile.js
+++ GoIT/markup/home/lesson-js-17-18/Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: 'img/',
                     src: ['**/*.{png,jpg,gif}'],
-                    dest: 'img/build/'
+                    dest: 'dest/img/build/'
                 }]
             }
         },
